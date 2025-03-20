65c0d8dcdf5ea926b660e43747b8d5f6405d8feb
Thanakrit <thanakrit.than.biz@gmail.com>
2025-03-20 10:37:27 +0700

    assignment complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greetingInputText, setGreetingInputText] = useState("");
+  const [greetingText, setGreetingText] = useState("");
+
+  const updateGreetingInputText = (event) => {
+    setGreetingInputText(event.target.value);
+  };
+
+  const updateGreetingText = () => {
+    setGreetingText(greetingInputText);
+  };
+
   return (
-    <>
-      <h1>Simple Greeting App 2</h1>
-    </>
+    <div className="App">
+      <h1>Simple Greeting Message App 2</h1>
+      <div className="greeting-container">
+        <h2>{greetingText}</h2>
+        <input
+          type="text"
+          value={greetingInputText}
+          onChange={updateGreetingInputText}
+        />
+        <div className="buttons">
+          <button onClick={updateGreetingText}>Add</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
